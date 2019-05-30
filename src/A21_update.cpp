a99e4dde3021aef6903cd2f3363fb3ad92674f75
jlw98 <joshua.warren@yale.edu>
2019-05-30 10:10:28 -0400

    Decimal update.

--- src/A21_update.cpp
+++ src/A21_update.cpp
@@ -12,7 +12,7 @@ double A21_update(arma::vec gamma_star,
                   arma::vec delta2,
                   double sigma2_A){
   
-double A21_var = 1/(sum(delta1%delta1) + (1.00/sigma2_A));
+double A21_var = 1.00/(sum(delta1%delta1) + (1.00/sigma2_A));
 double A21_mean = A21_var*sum(delta1%(gamma_star - A22*delta2));
 
 double A21 = R::rnorm(A21_mean, 
